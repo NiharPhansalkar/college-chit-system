42e7c2c02073229f831fba90bc3173ee095d32fb
Nihar Phansalkar <phansalkarnihar@gmail.com>
2022-11-30 12:02:49 +0000

    changed card type and made a flipping card

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-"use strict"
+const cardContent = document.querySelectorAll(".card-content");
 
-const cards = Array.from(document.querySelectorAll(".card"));
-
-for (let card of cards) {
-    card.addEventListener("click", () => {
-        console.log(card);
-        card.style.transform = "rotateY(180deg)";
-        card.addEventListener("transitionend", () => {
-            card.children[0].classList.remove("no-rotate-margin")
-            card.children[2].classList.remove("text-format-front")
-            card.children[0].classList.add("rotated-margin")
-            card.children[2].classList.add("text-format-back")
-        })
-    });
-}
+console.log(cardContent);
