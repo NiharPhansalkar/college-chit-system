e1ac9aff08646285286f928b54a4f3adb4d46ea8
Nihar Phansalkar <phansalkarnihar@gmail.com>
2023-01-01 11:14:16 +0000

    js: added comments in server file

--- js/server.js
+++ js/server.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 const server = http.createServer((req, res) => {
     if (req.url === "/") {
-        res.writeHead(200, { "Content-Type" : "text/html" });
+        res.writeHead(200, { "Content-Type" : "text/html" }); // Gives a response header, which is used to give more detail about the response
         fs.readFile("../index.html", (error, data) => {
             if (error) {
                 res.writeHead(404);
@@ -17,7 +17,7 @@ const server = http.createServer((req, res) => {
         });
     }else if (req.url.match(/.css$/)) {
         let cssPath = path.join(path.resolve(__dirname, "../"), req.url); 
-        let fileStream = fs.createReadStream(cssPath);
+        let fileStream = fs.createReadStream(cssPath); // creates a stream from where we can read our file
         res.writeHead(200, {"Content-Type" : "text/css"});
         fileStream.pipe(res);
     }else if (req.url.match(/.js$/)) {
