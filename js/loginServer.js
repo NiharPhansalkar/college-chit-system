2aa060a8bfc00c5840e6d164797cf8e9751aab53
Nihar Phansalkar <phansalkarnihar@gmail.com>
2023-01-02 16:14:58 +0000

    start of login page creation

---
+++ js/loginServer.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    if (req.url == "/") {
+        res.writeHead(200, { "Content-Type" : "text/html" }); // Gives a response header, which is used to give more detail about the response
+        fs.readFile("../loginPage.html", (error, data) => {
+            if (error) {
+                res.writeHead(404);
+                res.write("Error: Page not found");
+            }else {
+                res.end(data);
+            }
+        });
+    }else if (req.url.match(/.css$/)) {
+        const cssPath = path.join(path.resolve(__dirname, "../"), req.url);
+        let fileStream = fs.createReadStream(cssPath);
+        res.writeHead(200, {"Content" : "text/css"});
+        fileStream.pipe(res);
+    }
+});
+
+server.listen(port, (error) => {
+    if (error) {
+        console.log("Something went wrong");
+    } else {
+        console.log("Server listening on port " + port);
+    }
+})
