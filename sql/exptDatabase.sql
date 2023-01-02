f1c38207f82127e82a6e50bf6cff561d073be100
Nihar Phansalkar <phansalkarnihar@gmail.com>
2023-01-02 16:14:27 +0000

    db: created experiments table and relation between experiments and subjects tables

--- sql/exptDatabase.sql
+++ sql/exptDatabase.sql
@@ -11,6 +11,21 @@ create table faculty_subjects (
     faculty_id int not null,
     subject_id int not null,
     primary key (faculty_id, subject_id),
-    foreign key (faculty_id) references faculty_information (faculty_id),
-    foreign key (subject_id) references subjects (subject_id)
+    foreign key (faculty_id) 
+        references faculty_information (faculty_id),
+    foreign key (subject_id) 
+        references subjects (subject_id)
+);
+create table experiments (
+    experiment_id serial primary key,
+    experiment_description varchar(255) not null
+);
+create table subject_experiments (
+    subject_id int not null,
+    experiment_id int not null,
+    primary key (subject_id, experiment_id),
+    foreign key (subject_id) 
+        references subjects(subject_id),
+    foreign key (experiment_id) 
+        references experiments(experiment_id)
 );
