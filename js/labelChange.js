a776efc800fa6411eb22718ea8253001c808db21
Nihar Phansalkar <phansalkarnihar@gmail.com>
2023-01-04 12:40:45 +0000

    css, js: completed basic login page design

--- js/labelChange.js
+++ js/labelChange.js
@@ -1,15 +1,14 @@
 const inputElements = Array.from(document.querySelectorAll(".input-group input"));
 
+console.log(inputElements.length);
+
 for (let i = 0; i < inputElements.length; i++) {
-    if (inputElements[i].value !== "") {
-        inputElements[i].labels[0].classList.add("translate-label");
-    }else {
-        inputElements[i].labels[0].classList.remove("translate-label");
-    }
     inputElements[i].addEventListener("focus", () => {
         inputElements[i].labels[0].classList.add("translate-label");
     })
     inputElements[i].addEventListener("focusout", () => {
-        inputElements[i].labels[0].classList.remove("translate-label");
+        if (inputElements[i].value.length === 0) {
+            inputElements[i].labels[0].classList.remove("translate-label");
+        }
     })
 }
